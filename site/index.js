3a362adc9614251d684e17a00bfa3847cbae7c03
camielverdult <incredabox@gmail.com>
2021-12-13 17:30:34 +0100

    Update index.js

--- site/index.js
+++ site/index.js
@@ -205,7 +205,8 @@ function removeActiveFromButtons() {
             latestDataDiv.appendChild(temperateP);
         }
 
-        if (latestData[0].sensorData.lightLogscale !== null) {
+        if (latestData[0].sensorData.lightLogscale === null) {
+            // We have no lightLogscale, use the lightLux parameter instead
             let lightP = document.createElement("p");
             lightP.innerHTML = "Light: " + latestData[0].sensorData.lightLux + " Lux";
 
