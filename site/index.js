cd8558ae6e99afba996b33791866066b32f2a3bb
camielverdult <camiel@camiel.pw>
2021-12-14 12:20:07 +0100

    Insert chart

--- site/index.js
+++ site/index.js
@@ -49,6 +49,46 @@ async function getLatestForDeviceID(id) {
     return await fetchFromApi(`device/${id}/latest`);
 }
 
+async function showData() {
+
+const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+}
+
 async function showDevices(devicesCollection) {
     // This function will fill up the devices list with devices passed
     // as id: device in deviceCollection
@@ -284,5 +324,5 @@ async function getLatestForLocation(location) {
     allButton.innerHTML = "All"; // Change the name to be the city name
 
     citiesClass.appendChild(allButton);
-
+    await showData();
 })();
