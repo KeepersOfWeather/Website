09f0394eefb4f2bb53b75f12832665a4b5b950df
Max Thielen <497441@student.saxion.nl>
2021-12-17 12:55:41 +0100

    nvm

--- site/index.js
+++ site/index.js
@@ -167,7 +167,7 @@ async function showDevices(devicesCollection) {
     let devicesList = document.getElementsByClassName("devices");
 
     if (devicesList.length !== 0) {
-        devicesList = devicesList;
+        devicesList = devicesList[0];
     } else {
         console.log("Something is broken when finding devices ul...")
         return;
