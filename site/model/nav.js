540eb1afc1bf75cee438c31e3484cf4342a2e75b
Max Thielen <497441@student.saxion.nl>
2022-01-02 22:21:18 +0100

    fix import error

--- site/model/nav.js
+++ site/model/nav.js
@@ -55,7 +55,7 @@ async function createDeviceList(devicesCollection) {
     
 }
 
-export async function createNavBar() {
+var nav = async function createNavBar() {
     // Begin by asking API for all devices sorted by location
     // Generate tabs dynamically: https://www.w3schools.com/howto/howto_js_tabs.asp
     let cities = new Api('locations');
@@ -119,3 +119,5 @@ export async function createNavBar() {
     createDeviceList(allDevices.json);
     await updateLatestWeatherDiv();
 }
+
+export {nav}
