7997dec8b9a8fbf859db337abb2a6986cdaaf9c7
IlirM0 <499314@student.saxion.nl>
2022-01-06 00:29:56 +0100

    put max of date inputs to today

--- site/model/main.js
+++ site/model/main.js
@@ -1,8 +1,11 @@
 import {createNavBar} from './nav.js';
 import {createGraphs} from './graph.js';
+import {initDateInputFields} from './dateinput.js';
 
 (async () => {
     await createNavBar();
 
     await createGraphs(-1);
+
+    await initDateInputFields();
 })();
