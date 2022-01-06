f45489cbd5eab533b73614500c13c690db850ef0
IlirM0 <499314@student.saxion.nl>
2022-01-06 20:00:44 +0100

    added that the endDate can never be a lower value that beginDate

--- site/model/onchangeevents.js
+++ site/model/onchangeevents.js
@@ -1,9 +1,12 @@
-function endDateOnChange()
-{
-    console.log("End Date Changed");
-}
-
 function beginDateOnChange()
 {
     console.log("Begin Date Changed");
+    var firstdate = document.getElementById("beginDate").value;
+    // document.getElementById("endDate").value = firstdate;
+    document.getElementById("endDate").setAttribute("min",firstdate);
+}
+
+function endDateOnChange()
+{
+    console.log("End Date Changed");
 }
