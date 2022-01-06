f5cc2ab1f84248b03d81eecd7b1e945fd97594d2
IlirM0 <499314@student.saxion.nl>
2022-01-06 19:53:36 +0100

    let see if this works

--- site/model/main.js
+++ site/model/main.js
@@ -2,15 +2,7 @@ import {createNavBar} from './nav.js';
 import {createGraphs} from './graph.js';
 import {initDateInputFields} from './dateinput.js';
 
-function endDateOnChange()
-{
-    console.log("End Date Changed");
-}
 
-function beginDateOnChange()
-{
-    console.log("Begin Date Changed");
-}
 
 (async () => {
     await createNavBar();
---
+++ site/model/onchangeevents.js
@@ -0,0 +1,9 @@
+function endDateOnChange()
+{
+    console.log("End Date Changed");
+}
+
+function beginDateOnChange()
+{
+    console.log("Begin Date Changed");
+}
