8b1478a060026714361e3174a2e66157b0f50747
IlirM0 <499314@student.saxion.nl>
2022-01-06 20:48:39 +0100

    changed small thing

--- site/model/onchangeevents.js
+++ site/model/onchangeevents.js
@@ -1,9 +1,9 @@
 function fromDateOnChange()
 {
     console.log("from Date Changed");
-    var newMinDate = document.getElementById("beginDate").value;
-    // document.getElementById("endDate").value = newMinDate;
-    document.getElementById("endDate").setAttribute("min",newMinDate);
+    var newMinDate = document.getElementById("fromDate").value;
+    // document.getElementById("untillDate").value = newMinDate;
+    document.getElementById("untillDate").setAttribute("min",newMinDate);
 }
 
 function untillDateOnChange()
