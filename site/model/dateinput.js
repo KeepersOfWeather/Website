c51ff357a9af38be64b4420cbad013619843c706
IlirM0 <499314@student.saxion.nl>
2022-01-10 03:23:44 +0100

    added right information to inputs hopefully

--- site/model/dateinput.js
+++ site/model/dateinput.js
@@ -11,12 +11,46 @@ export function displayTimeInputs() {
     let selectFrom = document.createElement("div");
     let startDate = document.createElement("input");
 
+    // this code puts the latest date they can request to today.
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var mmMonthAgo = today.getMonth(); //the month of one month ago
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+    dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+    mmMonthAgo = '0' + mmMonthAgo;
+    mm = '0' + mm;
+    } 
+        
+    today = yyyy + '-' + mm + '-' + dd;
+    //document.getElementById("fromDate").setAttribute("max", today);
+    //document.getElementById("untillDate").setAttribute("max", today);
+    //// set standard value of endDate to today as well. 
+    //document.getElementById("untillDate").setAttribute("value", today);
+    //// the date value of beginDate is put to one month ago
+    todayMonthAgo = '';
+    if(mm == '01')
+    {
+        todayMonthAgo = (yyyy-1) + '-12-01';
+        //document.getElementById("fromDate").setAttribute("value", ((yyyy-1) + '-12-01'));
+    }
+    else
+    {
+        todayMonthAgo = yyyy + '-' + mmMonthAgo + '-01';
+        //document.getElementById("fromDate").setAttribute("value", (yyyy + '-' + mmMonthAgo + '-01'));
+    }
+
     startDate.type = "date";
     startDate.id = "fromDate";
     startDate.name = "fromDate";
-    startDate.value = "2018-07-22";
+    startDate.value = todayMonthAgo;
     startDate.min = "2021-10-01";
-    startDate.max = "2018-12-31";
+    startDate.max = today;
     startDate.onchange = async function fromDateOnChange() {
         console.log("from Date Changed");
         var newMinDate = document.getElementById("fromDate").value;
@@ -39,9 +73,9 @@ export function displayTimeInputs() {
     endDate.type = "date";
     endDate.id = "untillDate";
     endDate.name = "untillDate";
-    endDate.value = Date.today;
-    endDate.min = "2021-10-01";
-    endDate.max = "2018-12-31";
+    endDate.value = today;
+    endDate.min = todayMonthAgo;
+    endDate.max = today;
     endDate.onchange = async function untillDateOnChange(){
         console.log("untill Date Changed");
     }
@@ -76,18 +110,21 @@ export function initTimeInputs()
     } 
         
     today = yyyy + '-' + mm + '-' + dd;
-    document.getElementById("fromDate").setAttribute("max", today);
-    document.getElementById("untillDate").setAttribute("max", today);
-    // set standard value of endDate to today as well. 
-    document.getElementById("untillDate").setAttribute("value", today);
-    // the date value of beginDate is put to one month ago
+    //document.getElementById("fromDate").setAttribute("max", today);
+    //document.getElementById("untillDate").setAttribute("max", today);
+    //// set standard value of endDate to today as well. 
+    //document.getElementById("untillDate").setAttribute("value", today);
+    //// the date value of beginDate is put to one month ago
+    todayMonthAgo = '';
     if(mm == '01')
     {
-        document.getElementById("fromDate").setAttribute("value", ((yyyy-1) + '-12-01'));
+        todayMonthAgo = (yyyy-1) + '-12-01';
+        //document.getElementById("fromDate").setAttribute("value", ((yyyy-1) + '-12-01'));
     }
     else
     {
-        document.getElementById("fromDate").setAttribute("value", (yyyy + '-' + mmMonthAgo + '-01'));
+        todayMonthAgo = yyyy + '-' + mmMonthAgo + '-01';
+        //document.getElementById("fromDate").setAttribute("value", (yyyy + '-' + mmMonthAgo + '-01'));
     }
 
 }
