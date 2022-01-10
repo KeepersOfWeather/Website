2a8ee6acdcabfbeabc72f1117c44e277854bd713
IlirM0 <499314@student.saxion.nl>
2022-01-10 03:39:12 +0100

    removed init time inputs

--- site/model/dateinput.js
+++ site/model/dateinput.js
@@ -90,44 +90,44 @@ export function displayTimeInputs() {
     dateDiv.appendChild(selectTill);
 }
 
-export function initTimeInputs()
-{
+// export function initTimeInputs()
+// {
 
-    // this code puts the latest date they can request to today.
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
-    var mmMonthAgo = today.getMonth(); //the month of one month ago
-    var yyyy = today.getFullYear();
+//     // this code puts the latest date they can request to today.
+//     var today = new Date();
+//     var dd = today.getDate();
+//     var mm = today.getMonth() + 1; //January is 0!
+//     var mmMonthAgo = today.getMonth(); //the month of one month ago
+//     var yyyy = today.getFullYear();
 
-    if (dd < 10) {
-    dd = '0' + dd;
-    }
+//     if (dd < 10) {
+//     dd = '0' + dd;
+//     }
 
-    if (mm < 10) {
-    mmMonthAgo = '0' + mmMonthAgo;
-    mm = '0' + mm;
-    } 
+//     if (mm < 10) {
+//     mmMonthAgo = '0' + mmMonthAgo;
+//     mm = '0' + mm;
+//     } 
         
-    today = yyyy + '-' + mm + '-' + dd;
-    //document.getElementById("fromDate").setAttribute("max", today);
-    //document.getElementById("untillDate").setAttribute("max", today);
-    //// set standard value of endDate to today as well. 
-    //document.getElementById("untillDate").setAttribute("value", today);
-    //// the date value of beginDate is put to one month ago
-    todayMonthAgo = '';
-    if(mm == '01')
-    {
-        todayMonthAgo = (yyyy-1) + '-12-01';
-        //document.getElementById("fromDate").setAttribute("value", ((yyyy-1) + '-12-01'));
-    }
-    else
-    {
-        todayMonthAgo = yyyy + '-' + mmMonthAgo + '-01';
-        //document.getElementById("fromDate").setAttribute("value", (yyyy + '-' + mmMonthAgo + '-01'));
-    }
+//     today = yyyy + '-' + mm + '-' + dd;
+//     //document.getElementById("fromDate").setAttribute("max", today);
+//     //document.getElementById("untillDate").setAttribute("max", today);
+//     //// set standard value of endDate to today as well. 
+//     //document.getElementById("untillDate").setAttribute("value", today);
+//     //// the date value of beginDate is put to one month ago
+//     todayMonthAgo = '';
+//     if(mm == '01')
+//     {
+//         todayMonthAgo = (yyyy-1) + '-12-01';
+//         //document.getElementById("fromDate").setAttribute("value", ((yyyy-1) + '-12-01'));
+//     }
+//     else
+//     {
+//         todayMonthAgo = yyyy + '-' + mmMonthAgo + '-01';
+//         //document.getElementById("fromDate").setAttribute("value", (yyyy + '-' + mmMonthAgo + '-01'));
+//     }
 
-}
+// }
 
 // await function setEndDateToday()
 // {
