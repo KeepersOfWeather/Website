a15f27b84c699c582432ccfbe7364249804e7d69
Max Thielen <497441@student.saxion.nl>
2022-01-12 02:37:47 +0100

    test date input for graphs

--- site/model/main.js
+++ site/model/main.js
@@ -6,5 +6,5 @@ import {createGraphs} from './graph.js';
 (async () => {
     await createNavBar();
 
-    await createGraphs(-1, '2022-01-02 16:00:00', '2022-01-02 18:00:00');
+    await createGraphs(1, '2022-01-02 16:00:00', '2022-01-02 18:00:00');
 })();
