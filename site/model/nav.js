128ffe4c397abfa1f64d4363aee8ec659d60929d
Max Thielen <497441@student.saxion.nl>
2022-01-12 23:44:18 +0100

    maybe baby

--- site/model/nav.js
+++ site/model/nav.js
@@ -107,7 +107,7 @@ async function createDeviceList(devicesCollection) {
                     //         }
                     //       }
                     // }
-                    console.log(city);
+                    console.log(city[0].id);
                     if(city == "All"){
                         console.log("ALL CITY active");
                         if(newDeviceCheckBox.id == 0 || newDeviceCheckBox.id == 1){
