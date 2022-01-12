c0e3a2c2a40e368603e3250f6337698e750a3527
De0lieb0l <57093099+De0lieb0l@users.noreply.github.com>
2022-01-12 16:19:17 +0100

    i tried

--- site/model/main.js
+++ site/model/main.js
@@ -8,7 +8,5 @@ import {createGraphs} from './graph.js';
     await createGraphs(-1);
 
     await createGraphs(1, '2022-01-02 16:00:00', '2022-01-02 18:00:00');
-    await createGraphs(2, '2022-01-02 16:00:00', '2022-01-02 18:00:00');
-    await createGraphs(3, '2022-01-02 16:00:00', '2022-01-02 18:00:00');
-    await createGraphs(4, '2022-01-02 16:00:00', '2022-01-02 18:00:00');
+
 })();
