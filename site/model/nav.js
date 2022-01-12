81bba5af45ad1fc84f37957426da27fad356050d
Max Thielen <497441@student.saxion.nl>
2022-01-12 23:54:11 +0100

    maybe baby

--- site/model/nav.js
+++ site/model/nav.js
@@ -136,11 +136,9 @@ async function createDeviceList(devicesCollection) {
                     //     arrayOfCheckboxes[i].disabled = false;
                     //   }
                     if(numOfCheckboxesSelected == 0){
-                        console.log("enable all");
-                        if(arrayOfCheckboxes.length == 1) arrayOfCheckboxes[0].disabled = false;
-                        if(arrayOfCheckboxes.length == 2) arrayOfCheckboxes[1].disabled = false;
-                        if(arrayOfCheckboxes.length == 3) arrayOfCheckboxes[2].disabled = false;
-                        if(arrayOfCheckboxes.length == 4) arrayOfCheckboxes[3].disabled = false;
+                        for(let i=0; i<arrayOfCheckboxes.length; i++){
+                            arrayOfCheckboxes[i].disabled = false;
+                        }
                     }
                 }
                 let activeGraphs = new Array;
