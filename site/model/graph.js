2e5df9ef3fb28b537297f33769f191333b40acaa
Max Thielen <497441@student.saxion.nl>
2022-01-12 22:31:29 +0100

    funsies

--- site/model/graph.js
+++ site/model/graph.js
@@ -141,7 +141,7 @@ async function fillGraph(title, data, timestamps, ctx, title1, data1, title2, da
     // let _ = new Chart(ctx).Line(_data);
 }
 
-export async function createGraphs(id, redraw, startDate, endDate) {
+export async function createGraphs(ids, redraw, startDate, endDate) {
 
     var tempCanvas = document.getElementById('tempGraph');
     var humCanvas = document.getElementById('humGraph');
@@ -155,7 +155,7 @@ export async function createGraphs(id, redraw, startDate, endDate) {
     // if(endDate == null) endDate = getTodayString();
     // if(startDate == null) startDate = getTodayString(-1);
 
-    if (id === -1) {
+    if (ids === undefined || ids.length == 0) {
         let data = new Array;
         let timestamps = new Array;
         fillGraph('Temperature', data, timestamps, tempContext);
@@ -163,13 +163,32 @@ export async function createGraphs(id, redraw, startDate, endDate) {
         fillGraph('Light', data, timestamps, lightContext);
     }
     else{
-        let device = await initDevice(id, startDate, endDate);
-        fillGraph('Temperatures', device.temperature, device.timeStamps, tempContext);
-        if(device.pressure[0]===null) fillGraph('Humidity', device.humidity, device.timeStamps, humContext);
-        else fillGraph('Pressure', device.pressure, device.timeStamps, humContext);
-        fillGraph('Light', device.light, device.timeStamps, lightContext);
+        let device;
+        if(ids.length == 1) {
+            device = await initDevice(id[0], startDate, endDate);
+            fillGraph(device.name, device.temperature, device.timeStamps, tempContext);
+            if(device.pressure[0]===null) fillGraph(device.name, device.humidity, device.timeStamps, humContext);
+            else fillGraph(device.name, device.pressure, device.timeStamps, humContext);
+            fillGraph(device.name, device.light, device.timeStamps, lightContext);
+        }
+        else if(ids.length == 2){
+            device = await initDevice(id[0], startDate, endDate);
+            let device1 = await initDevice(id[1], startDate, endDate);
+            fillGraph(device.name, device.temperature, device.timeStamps, tempContext,device1.name,device1.temperature);
+            if(device.pressure[0]===null) fillGraph(device.name, device.humidity, device.timeStamps, humContext,device1.name,device1.humidity);
+            else fillGraph(device.name, device.pressure, device.timeStamps, humContext,device1.name,device1.pressure);
+            fillGraph(device.name, device.light, device.timeStamps, lightContext,device1.name,device1.light);
+        }
+        else if(ids.length == 3){
+            device = await initDevice(id[0], startDate, endDate);
+            let device1 = await initDevice(id[1], startDate, endDate);
+            let device2 = await initDevice(id[2], startDate, endDate);
+            fillGraph(device.name, device.temperature, device.timeStamps, tempContext,device1.name,device1.temperature,device2.name,device2.temperature);
+            if(device.pressure[0]===null) fillGraph(device.name, device.humidity, device.timeStamps, humContext,device1.name,device1.humidity,device2.name,device2.humidity);
+            else fillGraph(device.name, device.pressure, device.timeStamps, humContext,device1.name,device1.pressure,device2.name,device2.pressure);
+            fillGraph(device.name, device.light, device.timeStamps, lightContext,device1.name,device1.light,device2.name,device2.light);
+        }
     }
 
     if(!redraw) {displayTimeInputs(id);}
-    //initTimeInputs();  
 }
